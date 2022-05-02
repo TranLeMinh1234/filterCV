88cf8bf86fecd223af60b4697d148654c4e05f20
TranLeMinh1234 <tlMinh10300@gmail.com>
2022-05-03 02:44:49 +0700

    day code

---
+++ filterCV/Controllers/FilterCv.cs
@@ -0,0 +1,40 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filterCV.Controllers
+{
+    public class FilterCv : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult testApi()
+        {
+            return new JsonResult(new { data = "asdasd" });
+        }
+
+        [HttpPost("FilterCv/execute")]
+        public JsonResult filterCV(List<IFormFile> formFile)
+        {
+            PdfReader reader = new PdfReader(formFile[0].OpenReadStream());
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+            {
+                text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+            }
+
+            return new JsonResult(new { data = text.ToString() });
+        }
+
+    }
+}
